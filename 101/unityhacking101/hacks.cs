524ee57130e02c2ce69bcc7cfbe7d8dbd8b30916
dan <dan@mayer.cool>
2023-02-19 18:52:38 -0800

    add 101 project

---
+++ 101/unityhacking101/hacks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using System.Reflection;
+
+namespace hax
+{
+    public class Hacks : MonoBehaviour
+    {
+        // Cast a wide net with our BindingFlags to catch most variables we would run into. Scope this down as needed.
+        // https://learn.microsoft.com/en-us/dotnet/api/system.reflection.bindingflags?redirectedfrom=MSDN&view=net-7.0
+        BindingFlags flags = BindingFlags.Instance
+               | BindingFlags.Public
+               | BindingFlags.NonPublic
+               | BindingFlags.Static;
+
+        // Unity Engine reserved function that handles UI events for all objects
+        // Taken pretty much verbatim from https://docs.unity3d.com/ScriptReference/GUI.Window.html
+        public void OnGUI()
+        {
+            // Create a window at the center top of our game screen that will hold our button
+            Rect windowRect = new Rect(Screen.width / 2, Screen.height / 8, 120, 50);
+
+            // Register the window. Notice the 3rd parameter is a callback function to make the window, defined below
+            windowRect = GUI.Window(0, windowRect, DoMyWindow, "HackBox");
+
+            // Make the contents of the window
+            void DoMyWindow(int windowID)
+            {
+                // Combo line that creates the button and then also will check if it has been pressed
+                if (GUI.Button(new Rect(10, 20, 100, 20), "Add Tail"))
+                {
+                    this.AddTail();
+                }
+            }
+        }
+
+        public void AddTail()
+        {
+            // Get the instantiated Snake GameObject
+            Snake snake = GameObject.FindObjectOfType<Snake>();
+
+            // Create a "Type" object for the type "Snake"
+            Type snakeType = snake.GetType();
+            // Use System.Reflection.FieldInfo object to discover the attributes of the field and provide access to its metadata
+            // https://learn.microsoft.com/en-us/dotnet/api/system.reflection.fieldinfo?view=net-7.0
+            FieldInfo ateField = snakeType.GetField("ate", flags);
+            // Set the value of the "ate" field within our game's Snake object to true
+            ateField.SetValue(snake, true);
+
+            // MethodInfo/GetMethod is pretty much the same as FieldInfo/GetField for targeting methods in instantiated objects
+            MethodInfo dynMethod = snakeType.GetMethod("Move", flags);
+            // Call the "Move" method in our Snake object with no arguments
+            dynMethod.Invoke(snake, null);
+        }
+    }
+}
---
+++ 101/unityhacking101/loader.cs
@@ -0,0 +1,37 @@
+﻿//Boilerplate from https://www.unknowncheats.me/forum/unity/285864-beginners-guide-hacking-unity-games.html
+
+using System;
+// We will import this straight from the game files!
+using UnityEngine;
+// Our namespace, which we will create in another file
+using hax;
+
+
+namespace cheat
+{
+    public class Loader
+    {
+        public static GameObject L;
+        public static void Load()
+        {
+
+            // Create an instance of the Gameobject
+            Loader.L = new GameObject();
+
+            // Add our class that will be 
+            Loader.L.AddComponent<hax.Hacks>();
+
+            // Telling Unity not to destory our GameObject on level change
+            UnityEngine.Object.DontDestroyOnLoad(Loader.L);
+        }
+
+        public static void Unload()
+        {
+            // Destory our GameObject
+            UnityEngine.Object.Destroy(L);
+        }
+    }
+
+}
+
+
